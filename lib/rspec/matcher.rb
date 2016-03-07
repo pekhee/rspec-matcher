fa313235e883ad0ee190755fd364b95d8d813fca
Pooyan Khosravi <pekhee@gmail.com>
2016-03-07 09:03:10 +0800

    initial impl of RSpec::Matcher

--- lib/rspec/matcher.rb
+++ lib/rspec/matcher.rb
@@ -1,2 +1,157 @@
 require "rspec/matcher/identity"
 require "active_support/concern"
+
+module RSpec
+  # Provides minimal interface for creating RSpec Matchers.
+  #
+  # Register matcher with RSpec via `register_as`.
+  #
+  # Include `RSpec::Matchers::Composable` to support composable matchers.
+  #
+  # @example
+  #   class BeNil
+  #     include RSpec::Matcher
+  #     register_as "be_nil"
+  #
+  #     def match
+  #       return actual.nil?
+  #     end
+  #   end
+  #
+  #   expect(actual).to be_nil
+  #
+  # ## Required Methods:
+  # - `match`
+  #
+  # ## Optional Methods:
+  # - `description`
+  # - `failure_message`
+  # - `failure_message_when_negated`
+  # - `diffable?`
+  # - `supports_block_expectations?`
+  module Matcher
+    extend ActiveSupport::Concern
+
+    # To indicate no value was passed to matcher
+    UNDEFINED = Object.new.freeze
+
+    included do
+      prepend RSpec::Matcher::PrependedMethods
+
+      attr_accessor :expected, :actual
+    end
+
+    # @api private
+    # Used to let user define initialize
+    module PrependedMethods
+      # @api private
+      # @param [any] expected stored as expected and not passed to custom initializer
+      # @param [any] params... passed to custom initializer
+      # @param [Proc] block passed to custom initializer
+      def initialize expected = UNDEFINED, *args, &block
+        self.expected = expected
+        super(*args, &block)
+      end
+    end
+
+    # Methods added as class method to includer
+    module ClassMethods
+      # Registers this matcher in RSpec.
+      # @example
+      #   class BeNil
+      #     include RSpec::Matcher
+      #     register_as "be_nil"
+      #     #...
+      #   end
+      #
+      #   expect(actual).to be_nil
+      #
+      # @param [String] name what to register this matcher as
+      # @return [void]
+      def register_as name
+        s = self
+        m = Module.new do
+          define_method name do |*args, &block|
+            s.new(*args, &block)
+          end
+        end
+
+        RSpec.configure do |config|
+          config.include m
+        end
+      end
+    end
+
+    # @api private
+    # Hides RSpec internal api
+    def matches? actual
+      self.actual = actual
+      return match if method(:match).arity == 0
+
+      match actual
+    end
+
+    # @method actual
+    # @example expect(actual).to be(expected)
+    # @return [any] value passed to `expect()`
+
+    # @method expected
+    # @example expect(actual).to be(expected)
+    # @return [any] value passed to matcher function.
+
+    # @method initialize
+    # First argument passed to matcher is placed in expected and removed from
+    # arg list. Every argument beside that including a block is passed to
+    # initialize function.
+
+    # Determines if there is a match or not.
+    # @note Must be implemented.
+    # @param [any] actual same as actual method.
+    # @return [Boolean] true for success, false for failure.
+    def match _ = nil
+      raise "not implemented"
+    end
+
+    # Describes what this matcher does for composable matchers.
+    # @example be an string with X length
+    # @example match X regex
+    # @note raises by default
+    # @return [String]
+    def description
+      raise "not implemented"
+    end
+
+    # Describe failure.
+    # @example "expected EXPECTED to be of length X"
+    # @example "expected EXPECTED to match X"
+    # @note raises by default
+    # @return [String]
+    def failure_message
+      raise "not implemented"
+    end
+
+    # Describe failure when not_to is used.
+    # @example "expected EXPECTED not to be of length X"
+    # @example "expected EXPECTED not to match X"
+    # @note raises by default
+    # @return [String]
+    def failure_message_when_negated
+      raise "not implemented"
+    end
+
+    # Indicates if actual and expected should be diffed on failure.
+    # @note false by default
+    # @return [Boolean]
+    def diffable?
+      false
+    end
+
+    # Indicates if actual can be a block.
+    # @example expect { something }.not_to raise_error
+    # @note false by default
+    # @return [Boolean]
+    def supports_block_expectations?
+      false
+    end
+  end
+end
--- spec/lib/rspec/matcher_spec.rb
+++ spec/lib/rspec/matcher_spec.rb
@@ -1,13 +1,134 @@
 require "spec_helper"
 
 describe "RSpec::Matcher" do
-  subject do
+  let :to_be_nil_matcher do
     Class.new do
       include RSpec::Matcher
+
+      def match actual
+        return true if actual.nil?
+      end
     end
   end
 
-  it "should be instantiable" do
-    expect {subject.new}.not_to raise_error
+  subject :instance do
+    to_be_nil_matcher.new :expected_probe
+  end
+
+  it "is instantiable" do
+    expect { subject }.not_to raise_error
+  end
+
+  describe "#matches? " do
+    let(:matchable) { nil }
+    let(:not_matchable) { "anything else" }
+
+    it_behaves_like "is defined", name: :matches?
+
+    it "returns true for a match" do
+      expect(instance.matches?(matchable)).to be_truthy
+    end
+
+    it "returns false for failure" do
+      expect(instance.matches?(not_matchable)).to be_falsy
+    end
+  end
+
+  describe "#match" do
+    it "decides if there is a match" do
+      allow(instance).to receive(:match).with(not_important).and_return true
+      expect(instance.matches?(not_important)).to be_truthy
+    end
+
+    it "decides if there isn't a match" do
+      allow(instance).to receive(:match).with(not_important).and_return false
+      expect(instance.matches?(not_important)).to be_falsy
+    end
+
+    it "works with arity of 0" do
+      instance.instance_eval do
+        def match
+          raise "correct invocation"
+        end
+      end
+
+      expect { instance.matches? not_important }.to raise_error "correct invocation"
+    end
+  end
+
+  describe "#actual" do
+    it_behaves_like "is defined", name: :actual
+
+    it "is first arg of #matches? and accessible from #match" do
+      instance.instance_eval do
+        def match _
+          raise "wrong value" if actual != :actual_probe
+
+          true
+        end
+      end
+
+      expect { instance.matches? :actual_probe }.not_to raise_error
+    end
+  end
+
+  describe "#expected" do
+    it_behaves_like "is defined", name: :expected
+
+    it "is first arg of .new and accessible from #match" do
+      instance.instance_eval do
+        def match _
+          raise "wrong value" if expected != :expected_probe
+
+          true
+        end
+      end
+
+      expect { instance.matches? not_important }.not_to raise_error
+    end
+  end
+
+  describe "main interface" do
+    subject :instance do
+      Class.new do
+        include RSpec::Matcher
+      end.new not_important
+    end
+
+    [:match, :description, :failure_message, :failure_message_when_negated].each do |interface|
+      describe "##{interface}" do
+        it_behaves_like "default hook", name: interface, raises: "not implemented"
+      end
+    end
+  end
+
+  context "predicates interface" do
+    [:diffable?, :supports_block_expectations?].each do |predicate|
+      describe "##{predicate}" do
+        it_behaves_like "default hook", name: predicate, defaults_to: false
+      end
+    end
+  end
+
+  context "when registered with RSpec" do
+    before :all do
+      Class.new do
+        include RSpec::Matcher
+        register_as "be_nil"
+
+        def match
+          actual.nil?
+        end
+      end
+    end
+
+    it "is available as name passed to register as" do
+      expect(self).to respond_to :be_nil
+    end
+
+    it "works with or without passing expected" do
+      expect(be_nil(not_important).matches?(nil)).to be_truthy
+      expect(be_nil.matches?(nil)).to be_truthy
+    end
   end
 end
